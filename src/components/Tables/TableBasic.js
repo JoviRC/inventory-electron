0b2e37748385d676a9b1e1c8f7f9c0b8e238b847
jovirc <johanrc95@gmail.com>
2022-04-24 03:01:11 -0400

    lectura de permisos

--- src/components/Tables/TableBasic.js
+++ src/components/Tables/TableBasic.js
@@ -5,6 +5,8 @@ const axios = require("axios").default;
 
 const TableBasic = () => {
     const [data, setData] = React.useState([]);
+    const [authType, setAuthType] = React.useState(null);
+    const [userAuth, , setUserAuth] = React.useState(null);
 
     React.useEffect(() => {
         const decoder = jwt_decode(sessionStorage.getItem("token"));
@@ -12,6 +14,38 @@ const TableBasic = () => {
     }, []);
     React.useEffect(() => {
         const token = sessionStorage.getItem("token");
+        const email = JSON.parse(sessionStorage.getItem("userLog")).email;
+        axios
+            .get(_HOST_URL_ + "/users", {
+                headers: {
+                    Authorization: "Bearer " + token,
+                },
+                params: {
+                    email: email,
+                },
+            })
+            .then((res) => {
+                console.log(res.data[0]);
+                setUserAuth(res.data[0].account_type);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        axios
+            .get(_HOST_URL_ + "/roles", {
+                headers: {
+                    Authorization: "Bearer " + token,
+                },
+            })
+            .then((res) => {
+                const user = JSON.parse(sessionStorage.getItem("userLog"));
+                console.log(res.data[0].products);
+                setAuthType(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
         axios
             .get(_HOST_URL_ + "/products", {
                 headers: { Authorization: `Bearer ${token}` },
